b60e54772804b119f47887e6553b58b7fb4a15f5
fabianmatyas00 <fabian.matyas@students.jedlik.eu>
2024-10-11 13:07:25 +0200

    hazi kesz hurra :3

--- heron/main.py
+++ heron/main.py
@@ -1,6 +1,5 @@
 import random
-
-
+import math
 def main() -> None:
     # kérjük be a haromszogek számát (n)
     # addig ismeteljuk ameddig nem teljesül a 5 <= n <= 100 felt.
@@ -17,9 +16,18 @@ def main() -> None:
     n: int = int(input("háromszögek száma: "))
     while n < 5 or n > 100:
         n: int = int(input("háromszögek száma (5 <= n <= 100): "))
-    a: float = random.uniform(1, 100)
-    b: float = random.uniform(1, 100)
-    c: float = random.uniform(1, 100)
+    for i in range(n):
+        a: float = round(random.uniform(1, 100), 2)
+        b: float = round(random.uniform(1, 100), 2)
+        c: float = round(random.uniform(1, 100), 2)
+        while a + b < c or a + c < b or b + c < a:
+            a: float = round(random.uniform(1, 100), 2)
+            b: float = round(random.uniform(1, 100), 2)
+            c: float = round(random.uniform(1, 100), 2)
+        k: float = a + b + c
+        s: float = k / 2
+        t: float = math.sqrt(s * (s - a) * (s - b) * (s - c))
+        print(f"{i + 1}. háromszög ({a}, {b}, {c}): t = {round(t, 2)} , k = {round(k, 2)}")
 
 
 if __name__ == "__main__":
