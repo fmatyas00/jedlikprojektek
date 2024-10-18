2b78effb9a7e7d47bffdfb60a54700b9190ce952
fabianmatyas00 <fabian.matyas@students.jedlik.eu>
2024-10-18 19:45:19 +0200

    a fáraó rejtélyes feladványa...

--- tokeletes_szamok/main.py
+++ tokeletes_szamok/main.py
@@ -1,14 +1,23 @@
+import time
+
+
 def ez_tökéletes(vszam: int) -> bool:
-    for i in range(1, vszam):
-        if vszam % i == 0:
-            Sum = Sum + i
-    if Sum == vszam:
+    osztokszama = sum(i for i in range(1, vszam) if vszam % i == 0)
+    if vszam == osztokszama:
         return True
     else:
         return False
 
+
 def main() -> None:
-    pass  # keresd meg és ird a képernyőre az első négy tökéletes számot
+    t1 = time.time()
+    vszam: int = 0  # keresd meg és ird a képernyőre az első négy tökéletes számot
+    for vszam in range(1, 10000):
+        if ez_tökéletes(vszam):
+            print(f"tökéletes szám: {vszam}")
+    print("ennyi! nincs több!")
+    t2 = time.time()
+    print(f"program lefutásának ideje {round(t2 - t1, 3)}s. :)")
 
 
 if __name__ == "__main__":
