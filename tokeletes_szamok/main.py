49768e775ee6974b7e822df4771a7466fb711220
fabianmatyas00 <fabian.matyas@students.jedlik.eu>
2024-10-22 13:53:08 +0200

    skibidi toilet

--- tokeletes_szamok/main.py
+++ tokeletes_szamok/main.py
@@ -10,14 +10,18 @@ def ez_tökéletes(vszam: int) -> bool:
 
 
 def main() -> None:
+    Mylist: list[int] = []
     t1 = time.time()
     vszam: int = 0  # keresd meg és ird a képernyőre az első négy tökéletes számot
     for vszam in range(1, 10000):
         if ez_tökéletes(vszam):
-            print(f"tökéletes szám: {vszam}")
+            Mylist.append(vszam)
+            print("!")
     print("ennyi! nincs több!")
     t2 = time.time()
-    print(f"program lefutásának ideje {round(t2 - t1, 3)}s. :)")
+    print(
+        f"tökéletes számok: {Mylist} \n program lefutásának ideje {round(t2 - t1, 3)}s. :)"
+    )
 
 
 if __name__ == "__main__":
