92a1e0735119024883b1874218a945e36f7149b4
fabianmatyas00 <fabian.matyas@students.jedlik.eu>
2024-10-24 08:43:20 +0200

    most mi van ha megölöm magam

--- tokeletes_szamok/main.py
+++ tokeletes_szamok/main.py
@@ -2,25 +2,28 @@ import time
 
 
 def ez_tökéletes(vszam: int) -> bool:
-    osztokszama = sum(i for i in range(1, vszam) if vszam % i == 0)
-    if vszam == osztokszama:
+    osztók_összege = sum(i for i in range(1, vszam) if vszam % i == 0)
+    if osztók_összege == vszam:
         return True
     else:
         return False
 
 
 def main() -> None:
-    Mylist: list[int] = []
+    lista: list[int] = []
     t1 = time.time()
-    vszam: int = 0  # keresd meg és ird a képernyőre az első négy tökéletes számot
-    for vszam in range(1, 10000):
+    vszam: int = 1
+    tökéletes_számok_db: int = 0
+    while tökéletes_számok_db < 4:
         if ez_tökéletes(vszam):
-            Mylist.append(vszam)
             print("!")
+            lista.append(vszam)
+            tökéletes_számok_db += 1
+        vszam += 1
     print("vége")
     t2 = time.time()
     print(
-        f"tökéletes számok: {Mylist} \nprogram lefutásának ideje {round(t2 - t1, 3)}s. :)"
+        f"tökéletes számok: {lista} \nprogram lefutásának ideje {round(t2 - t1, 3)}s. :)"
     )
 
 
