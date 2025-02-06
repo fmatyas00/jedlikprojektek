452b4dd4d0cad622f75269df85ba865fe3aeda1e
fabianmatyas00 <fabian.matyas@students.jedlik.eu>
2025-02-06 14:28:52 +0100

    vegeztem

--- magyarorszag_hegyei/main.py
+++ magyarorszag_hegyei/main.py
@@ -6,6 +6,15 @@ def main() -> None:
     print(f"3. feladat: Hegycsúcsok száma: {len(m.hegycsucsok)} db")
     print(f"4. feladat: Hegycsúcsok átlagos magassága: {m.atlag_mag} m")
     print(f"5. feladat A legmagasabb hegycsúcs adatai:{m.legmagasabb_adatok}")
-
+    bevitel: int = int(input("Kérek egy magasságot: "))
+    if m.van_e_magasabb_borzsony(bevitel):
+        print(f"van {bevitel}m-nél magasabb hegycsúcs a Börzsönyben!")
+    else:
+        print(f"nincs {bevitel}m-nél magasabb hegycsúcs a Börzsönyben!")
+    print(f"7. feladat: 3000 labnal magasabb hegycsucsok szama: {m.haromezer_labnal_magasabb}")
+    print(f"8. feladat: Hegység statisztika {m.hegystat}")
+    with open("bukk-videk.txt", "w", encoding="utf-8") as file:
+        file.writelines(m.sorokiras)
+    print("9. feladat: bukk-videk.txt")
 if __name__ == "__main__":
     main()
--- magyarorszag_hegyei/megoldas.py
+++ magyarorszag_hegyei/megoldas.py
@@ -31,3 +31,48 @@ class Megoldas:
             if akt_max == i.magassag:
                 out = f"\n\tnév: {i.hegycsucs_neve}\n\tHegység: {i.hegyseg}\n\tMagasság: {i.magassag}"
         return out
+
+    def van_e_magasabb_borzsony(self, bevitel: int) -> bool:
+        for i in self._hegycsucsok:
+            if bevitel > i.magassag and i.hegyseg == "Börzsöny":
+                return False
+        return True
+
+    def magassag_lab(self, meter: int) -> float:
+        return meter * 3.280839895
+
+    @property
+    def haromezer_labnal_magasabb(self) -> int:
+        counter: int = 0
+        for i in self._hegycsucsok:
+            if self.magassag_lab(i.magassag) > 3000:
+                counter += 1
+        return counter
+
+    @property
+    def hegystat(self) -> str:
+        matra: int = 0
+        bukk: int = 0
+        borzsony: int = 0
+        zemplen: int = 0
+        koszeg: int = 0
+        for i in self._hegycsucsok:
+            if i.hegyseg == "Mátra":
+                matra += 1
+            elif i.hegyseg == "Bükk-vidék":
+                bukk += 1
+            elif i.hegyseg == "Börzsöny":
+                borzsony += 1
+            elif i.hegyseg == "Zempléni-hegység":
+                zemplen += 1
+            elif i.hegyseg == "Kőszegi-hegység":
+                koszeg += 1
+        return f"\n\tMátra - {matra} db\n\tBükk-vidék - {bukk} db\n\tBörzsöny - {borzsony} db\n\tZempléni-hegység - {zemplen} db\n\tKőszegi-hegység - {koszeg} db"
+
+    @property
+    def sorokiras(self):
+        sorok: list[str] = ["Hegycsúcs neve;Magasság láb\n"]
+        for i in self._hegycsucsok:
+            if i.hegyseg == "Bükk-vidék":
+                sorok.append(f"{i.hegycsucs_neve};{self.magassag_lab(i.magassag)}\n")
+        return sorok
